7586ae38f99e62ba4256202b507ba6a294d36dc8
D <haydend4@gmail.com>
2020-05-01 12:22:45 -0700

    found lowest cost, need cpyconstruct for Path class

--- TravelingSalesPerson.cpp
+++ TravelingSalesPerson.cpp
@@ -1,8 +1,10 @@
 #include "Path.cpp"
 
+#include <fstream>
 #include <iostream>
 #include <stack>
 #include <string>
+#include <sstream>
 #include <algorithm>
 
 using namespace std;
@@ -14,7 +16,7 @@ private:
 	int** 	adjacencyMatrix;
 	int 	vertexCount;
 	int 	minPathCost;
-	Path 	minPath;
+	Path* 	minPath;
 
 	// int adjacencyMatrix[numVertices][numVertices]
 	// {
@@ -27,32 +29,92 @@ private:
 	
 
 public:
-	GraphPlaceholder(int numVertex, string* newVertices)
+	GraphPlaceholder(int numVertex, string* newVertices, int** adjMatrix):vertexCount(numVertex)
 	{
+		vertices = new string[numVertex];
 
+		for(int i =0;i<numVertex;i++)
+		{
+			vertices[i] = newVertices[i]; 
+		}
+
+		setMatrix(numVertex, adjMatrix);
 	}
+
+	void setMatrix( int numVertices, int** adjMatrix )
+	{
+		adjacencyMatrix = new int* [numVertices];
+		for(int a=0;a<numVertices;a++)
+		{
+			adjacencyMatrix[a] = new int [numVertices]; 
+		}
+		
+		for(int i=0; i<numVertices; i++)
+		{
+			for(int j=0; j<numVertices; j++)
+			{
+				adjacencyMatrix[i][j] = adjMatrix[i][j];
+			}
+		}
+	}
+
+	//sets vertex array in adj array given vertex index and connection list(array)
 	bool setVertexConnections(int vertexIndex, int* connections)
 	{
+		for(int i = 0;i<vertexCount;i++)
+		{
+			adjacencyMatrix[vertexIndex][i] = connections[i];
+		}
 
+		return true;
 	}
-	string getConnectionValue(int vertexIndex, int connectionIndex)
+
+	//returns edge cost given vertex index and connecting vertex index
+	int getConnectionValue(int vertexIndex, int connectionIndex)
 	{
-
+		return adjacencyMatrix[vertexIndex][connectionIndex];
 	}
+
+	//returns name of vertex given the index
+	string getVertexName(int vertexIndex)
+	{
+		return vertices[vertexIndex];
+	}
+
 	int traversePath(Path currentPath)
 	{
+		int sum = 0;
 
+		for(int i=0;i<vertexCount+1;i++)
+		{
+			sum += getConnectionValue( currentPath.getCityIndexAt(i),currentPath.getCityIndexAt(i+1) );
+		}
+
+		return sum;
 	}
-}
+
+	void displayMatrix()
+	{
+
+		for(int i=0; i<vertexCount; i++)
+		{
+			for(int j=0; j<vertexCount; j++)
+			{
+				cout<<"Matrix["<<i<<"]["<<j<<"]="<<adjacencyMatrix[i][j]<<" , ";
+			}
+			cout<<endl;
+		}
+	}
+};
 
 
-
+/*Graph*/GraphPlaceholder ReadGraphFromFile(string filename);
 stack<Path> generatePaths(int numVertices);
 void displayPaths( stack<Path> pathsToDisplay, int numVertices );
 
 int main()
 {
-	
+	string inputFile = "input.txt";
 
 	//important to most of generation
 	int vertexCount = 5;
@@ -62,7 +124,155 @@ int main()
 	flapjacks = generatePaths(vertexCount);
 	displayPaths( flapjacks, vertexCount );
 
+	ReadGraphFromFile(inputFile).displayMatrix();
+
+
+	int lowestPathCost = 1000000;
+	Path* lowestPath;
+
+	int pathCount = 1;
+	while( !flapjacks.empty() )
+	{	
+		int pathCost;
+		cout<<"Path "<<pathCount<<": [";
+		for(int i=0;i<vertexCount+1;i++)
+		{
+			cout<<flapjacks.top().getCityIndexAt(i);
+			if(i != vertexCount)
+			{
+				cout<<",";
+			}
+			else
+			{
+				cout<<"]"<<endl;
+			}
+		}
+		pathCost = ReadGraphFromFile(inputFile).traversePath( flapjacks.top() );
+		cout<<"Path Cost: "<<pathCost<<endl;
+
+		if(pathCost<lowestPathCost)
+		{
+			lowestPathCost = pathCost;
+			//lowestPath = flapjacks.top(); //**** need to write Path copy constructor and possibly =operator overload 
+		}
+		flapjacks.pop();
+		pathCount++;
+	}
+
+	cout<<"Lowest Path Cost!: "<<lowestPathCost<<endl;
+	
+
 	return 0;
+}
+
+GraphPlaceholder ReadGraphFromFile(string filename)
+{
+	bool gotVertices = false;
+	int countVertices = 0;
+	int vertexIndex = 0;
+	string* vertices;
+	string lineIn;
+	string lineParse;
+	int** inMatrix;
+	
+	ifstream getGraph;
+	getGraph.open(filename);
+
+	getline(getGraph,lineIn,'\n');
+	stringstream getVerticesCount(lineIn);
+
+	//get vertices count
+	while( getline(getVerticesCount,lineParse,',') )
+	{
+		countVertices++;
+	}
+
+	// cout<<"countVertices: "<<countVertices<<endl;
+
+	vertices = new string [countVertices];
+
+	//setup empty 2D matrix of size (countVertices)^2
+	inMatrix = new int*[countVertices];
+	for(int i=0;i<countVertices;i++)
+	{
+		inMatrix[i] = new int[countVertices];
+	}
+
+	//accounting for vertex title row
+	int row = -1;
+	int column = -countVertices;
+
+	//reset getline to beginning of file
+	getGraph.seekg(0);
+
+	if( getGraph.is_open() )
+	{
+		
+		while( getline(getGraph,lineIn,'\n') )
+		{
+			stringstream lineStr(lineIn);
+
+			//accounts for vertex title columns
+			if(gotVertices)
+			{
+				column = 0;
+			}
+
+			while( getline(lineStr,lineParse,',') )
+			{
+				
+				if(!gotVertices)
+				{
+					//set vertices[0-n]
+					vertices[vertexIndex]=lineParse;
+					vertexIndex++;
+				}
+				else
+				{	
+					inMatrix[row][column] = stoi(lineParse);
+				}
+
+				column++;
+			}
+			//should get vertices after first iteration
+			gotVertices = true;
+			
+			row++;
+		}			
+	}
+
+	getGraph.close();
+
+	// cout<<"row: "<<row<<", col: "<<column<<endl;
+
+	
+	//check vertices Array
+	// cout<<"Vertex: ";
+	// for(int i=0;i<countVertices;i++)
+	// {
+	// 	 cout<<vertices[i];
+	// 	 if(i != (countVertices-1))
+	// 	 {
+	// 	 	cout<<",";
+	// 	 } 
+	// }
+	// cout<<endl;
+	// cout<<"}"<<endl;   //*******cout is fked when using "}" or "]"
+		 
+	
+	// check 2d matrix
+	// for(int i=0; i<countVertices; i++)
+	// {
+	// 	for(int j=0; j<countVertices; j++)
+	// 	{
+	// 		cout<<"Matrix["<<i<<"]["<<j<<"]="<<inMatrix[i][j]<<" , ";
+	// 	}
+	// 	cout<<endl;
+	// }
+
+	GraphPlaceholder inGraph(countVertices, vertices, inMatrix);
+
+	return inGraph;
 }
 
 //**********notes for automating exclusion, though seems unnecessary *********************
